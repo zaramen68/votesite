df9500caeac38ad8adbde47f9e9ab879efa7d35f
zaram <zaramenskiy@awada.ru>
2021-04-30 17:12:50 +0300

    details.html

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+       latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+   
+    question = get_object_or_404(Question, pk=question_id)
+    
+    return render(request, 'polls/detail.html', {'question': question})
